81f1895b9f01d362cf9586e5fdeedc40e72f06d4
Mauro Zegarra <mauro.zegarra@gmail.com>
2019-11-02 15:04:29 -0500

    Change method name

--- app/src/main/java/com/globomantics/login/MainActivity.kt
+++ app/src/main/java/com/globomantics/login/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        if (loginVerification.verify(email.text.toString(), password.text.toString()))
+        if (loginVerification.verifyCredentials(email.text.toString(), password.text.toString()))
             Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
         else
             Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
--- loginverification/src/main/java/com/globomantics/loginverification/LoginVerification.kt
+++ loginverification/src/main/java/com/globomantics/loginverification/LoginVerification.kt
@@ -1,7 +1,7 @@
 package com.globomantics.loginverification
 
 class LoginVerification {
-    fun verify(email: String, password: String): Boolean {
+    fun verifyCredentials(email: String, password: String): Boolean {
         return (email == "test@globomantics.com" &&
                 password == "test123") ||
                 (email == "admin@globomantics.com" &&
